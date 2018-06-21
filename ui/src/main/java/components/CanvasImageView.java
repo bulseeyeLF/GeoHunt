95b62783feb86a5487b7bcd28ac98ace39ffa65d
lanicaca <milanadundic96@hotmail.com>
2018-06-21 11:41:26 +0200

    Omg zaboravih ovo da pushnem, ulepsavanje

--- ui/src/main/java/components/CanvasImageView.java
+++ ui/src/main/java/components/CanvasImageView.java
@@ -4,6 +4,7 @@ package components;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import lombok.Getter;
@@ -12,18 +13,21 @@ import org.apache.log4j.Logger;
 
 public class CanvasImageView extends Canvas {
     @Getter
-    private MapImageView mapImageView;
+    private ImageView mapImageView;
     private boolean dragged;
     final private GraphicsContext graphicsContext = this.getGraphicsContext2D();
     private static Logger log = Logger.getLogger(CanvasImageView.class);
     private static double WIDTH = 500;
     private static double HEIGHT = 500;
+    private Image currentImage;
+    private double x=0,y =0;
 
 
     public CanvasImageView(Image img){
         super(WIDTH,HEIGHT);
+        currentImage = img;
         dragged = false;
-        mapImageView = new MapImageView(img);
+        mapImageView = new ImageView(img);
         graphicsContext.drawImage(img, 0, 0, WIDTH,HEIGHT);
 
         this.addEventHandler(MouseEvent.ANY,
@@ -38,18 +42,28 @@ public class CanvasImageView extends Canvas {
     public void setMapImageView(Image imageView){
         graphicsContext.clearRect(0,0,WIDTH,HEIGHT);
         mapImageView.setImage(imageView);
+        currentImage = imageView;
         graphicsContext.drawImage(imageView,0,0,WIDTH,HEIGHT);
     }
 
     public void DragDetected(MouseEvent event) {
+        graphicsContext.clearRect(0,0,WIDTH,HEIGHT);
+        draw();
         graphicsContext.setFill(Color.BLACK);
-        graphicsContext.strokeLine(event.getX(),event.getY(), event.getX(),event.getY());
+        graphicsContext.strokeLine(x,y, event.getX(),event.getY());
         log.debug("Mouse dragged");
         dragged = true;
 
     }
 
+    private void draw() {
+        graphicsContext.drawImage(currentImage, 0, 0, WIDTH,HEIGHT);
+
+    }
+
     public void MouseClicked(MouseEvent event) {
+        x = event.getX();
+        y = event.getY();
         log.debug("Mouse Clicked");
     }
 
