b0ce0bef29e069f6d2dd7d56e54f65ac3715afb3
lanicaca <milanadundic96@hotmail.com>
2018-06-22 18:45:11 +0200

    some fancy glowes and shadows, i think you're gonna like this :D

--- ui/src/main/java/components/Shapes.java
+++ ui/src/main/java/components/Shapes.java
@@ -1,8 +1,13 @@
 package components;
 
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Glow;
+import javafx.scene.effect.Shadow;
+import javafx.scene.paint.*;
 import javafx.scene.shape.Shape;
+import javafx.scene.shape.StrokeLineJoin;
+import javafx.scene.shape.StrokeType;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.log4j.Logger;
@@ -23,16 +28,23 @@ public abstract class Shapes {
 
     public void setSelected(boolean selected) {
         if (selected){
-            shape.setOpacity(0.5);
-            shape.setStroke(Color.BLACK);
-            shape.setStrokeDashOffset(5);
-            shape.setFill(Color.RED);
+            shape.setOpacity(0.6);
+            Glow glow = new Glow();
+            glow.setLevel(200);
+            glow.setInput(new DropShadow(15,Color.BLACK));
+            shape.setEffect(glow);
+            shape.setStroke(Color.DARKGRAY);
+            shape.setStrokeType(StrokeType.OUTSIDE);
+            shape.setStrokeWidth(1);
+            shape.setFill(Color.DARKGRAY);
             this.selected = true;
         }
         else {
-            shape.setOpacity(0.1);
+            shape.setEffect(null);
+            shape.setOpacity(0.4);
             shape.setStroke(Color.BLACK);
-            shape.setFill(Color.BLACK);
+            shape.setFill(Color.DARKGRAY);
+            shape.setStrokeWidth(1);
             this.selected = false;
         }
     }
@@ -41,16 +53,19 @@ public abstract class Shapes {
         shape.setOnMouseEntered(event -> {
             if (!isSelected()) {
                 shape.setOpacity(0.5);
-                shape.setFill(Color.BLACK);
+                shape.setFill(Color.DARKGRAY);
                 shape.setStrokeDashOffset(0);
-                shape.setStroke(Color.BLACK);
+                shape.setStroke(Color.DARKGRAY);
+                shape.setEffect(new DropShadow());
                 log.debug("entered");
                 event.consume();
             }
         });
         shape.setOnMouseExited(event -> {
             if (!isSelected()) {
-                shape.setOpacity(0.1);
+                shape.setOpacity(0.4);
+                shape.setEffect(null);
+                shape.setStroke(Color.BLACK);
                 log.debug("exited");
                 event.consume();
             }
